1bbf9f8bbeda47f85f490df8974f3d3c6e35ae39
Intizar Ahmad <intizarahmad@gmail.com>
2019-02-03 12:26:40 +0530

    custom timer function

---
+++ timers.js
@@ -0,0 +1,16 @@
+let loopArray = [1,2,3,4,5];
+var startTime = Date.now();
+var WAIT_TIME=2000;
+function customWait(n, callback){
+    let currentTime = Date.now();
+    while(startTime + n*WAIT_TIME > currentTime ){
+        currentTime = Date.now();
+    }
+    callback(n);
+}
+
+loopArray.forEach(n => {
+    customWait(n, function(n){
+        console.log(n);
+    });
+});
